8937e6ae2e6535a248b426ff928b448bfbde1732
SamratChakraborty99 <chakrabortysam1999@gmail.com>
2026-03-10 01:47:53 +0530

    First Commit message

---
+++ Python_Start.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+# So in-order to run this code, you need to have a Python version installed in the computer.
+# You can download it from the official website: https://www/python.org/downloads/
+# After installing the python, you can run this code by opening a terminal or a command prompt terminal 
+# In the powershell terminal, you need to navigate to the folder and then run the command Python Python_Start.py
+# If you are using the command prompt terminal, you can run the command python Python_Start.py
+# If you are using the python interactive shell, you can simply copy and paste the code and then press enter to run it.
